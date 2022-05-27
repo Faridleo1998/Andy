9800b12d5740951a8e990710294618876aace9df
Farid Moreno <farid.leonardo98@gmail.com>
2022-05-26 22:06:54 -0500

    Desactivar el scroll al abrir el menu mobil

--- js/app.js
+++ js/app.js
@@ -4,15 +4,18 @@
 const navMenu = document.getElementById('navMenu');
 const navToggle = document.getElementById('navToggle');
 const navLinks = document.querySelectorAll('.nav__link');
+const body = document.querySelector('body');
 
 navToggle.addEventListener('click', () => {
     navMenu.classList.toggle('active');
     navToggle.classList.toggle('active');
+    body.classList.toggle('no-scroll');
 })
 
 navLinks.forEach(navLink => {
     navLink.addEventListener('click', () => {
         navMenu.classList.toggle('active');
         navToggle.classList.toggle('active');
+        body.classList.toggle('no-scroll');
     })
 });
