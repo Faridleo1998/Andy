41ddac1e5bb086af6eb9c80ba32302e69bb80bc8
Farid Moreno <farid.leonardo98@gmail.com>
2022-06-07 20:44:44 -0500

    Cambia el color del sheader al realizar scroll

--- js/app.js
+++ js/app.js
@@ -1,11 +1,37 @@
 'use strict';
 
+// Swiper Js
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+});
+
+// Change backgroud header
+const scrollHeader = () => {
+    const header = document.getElementById('header');
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 50) {
+        header.classList.add('scroll-header');
+    } else {
+        header.classList.remove('scroll-header');
+    }
+}
+
+window.addEventListener('scroll', scrollHeader);
+
 // nav variables
 const navMenu = document.getElementById('navMenu');
 const navToggle = document.getElementById('navToggle');
 const navLinks = document.querySelectorAll('.nav__link');
 const body = document.querySelector('body');
 
+// Show or close mobile menu
 navToggle.addEventListener('click', () => {
     navMenu.classList.toggle('active');
     navToggle.classList.toggle('active');
@@ -18,16 +44,4 @@ navLinks.forEach(navLink => {
         navToggle.classList.toggle('active');
         body.classList.toggle('no-scroll');
     })
-});
-
-// Swiper Js
-const swiper = new Swiper(".swiper", {
-    // Optional parameters
-    loop: true,
-
-    // Navigation arrows
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    }
 });
